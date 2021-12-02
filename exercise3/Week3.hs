d169b21bad5fce1d201d85e02c7b148a55f517e8
Mujahid <hypercyte@gmail.com>
2021-12-02 18:13:39 +0000

    6: maybes

--- exercise3/Week3.hs
+++ exercise3/Week3.hs
@@ -13,3 +13,16 @@ safeDiv :: Int -> Int -> Maybe Int
 safeDiv x y
     | y == 0    = Nothing
     | otherwise = Just (div x y)
+
+-- just result: only if BOTH args are just
+-- nothing result: if EITHER arg is nothing
+pairMaybe :: Maybe a -> Maybe b -> Maybe (a,b)
+pairMaybe Nothing Nothing = Nothing
+pairMaybe (Just x) Nothing = Nothing
+pairMaybe Nothing (Just y) = Nothing
+pairMaybe (Just x) (Just y) = Just (x,y)
+
+-- same thing but since theres 3 nothings it could be similied to this!
+PairMaybe2 :: Maybe a -> Maybe b -> Maybe (a,b)
+pairMaybe2 (Just x) (Just y) = Just (x,y)
+pairMaybe2 _ _ = Nothing
