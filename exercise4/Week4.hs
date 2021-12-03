5efba88829073b629d7aca1ceb3711dda345b207
Mujahid <hypercyte@gmail.com>
2021-12-03 17:40:12 +0000

    task 7 8

--- exercise4/Week4.hs
+++ exercise4/Week4.hs
@@ -1,6 +1,7 @@
 module Week4 where
 
 import Data.Char
+import Data.List
 
 tripleAll :: [Int] -> [Int]
 tripleAll ns = [3*n | n <- ns]
@@ -41,6 +42,30 @@ divisors n = [ d | d <- [1..n], n `mod` d == 0]
 -- d is assigned list of ints from 1 to n
 -- then we do the input n and mod by each number in the list of d
 -- every time it's 0 means its divisible by n
+
+average :: [Double] -> Double
+average ds = sum ds / fromIntegral (length ds)
+-- sum of all the doubles in the list
+-- divided by
+-- length of the list (convered to double (thats what fromIntegral does))
+
+palindrome :: String -> Bool
+palindrome w = reverse w == w
+-- checks if input string is equal when reversed
+
+palindromeX :: String -> Bool
+palindromeX w = reverse (capitaliseLetters w) == capitaliseLetters w
+-- same thing as palindrome but ignores anything besides letters
+-- which allows for palindromes such as "Madam I'm Adam" to be considered
+-- true.
+
+frequency :: [Char] -> [(Char, Int)]
+frequency cs = [(head g, length g) | g <- group (sort cs)]
+-- head g = first character in each string in list
+-- length g = length of each string in list
+-- g <- group (sort cs) = g is a list of sorted and grouped ver of input
+-- like ["1","aa","b","cccc"] if input was "bcacbc1c"
+
 
 
 
