21eb79f3733bb3778f4ec4f19fec0c4559ef13d2
Mujahid <hypercyte@gmail.com>
2021-12-04 16:59:19 +0000

    tasks 1 and 2

--- exercise6/Week6Exercises.hs
+++ exercise6/Week6Exercises.hs
@@ -6,3 +6,10 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 
 -- Use Set to implement a fucntion
+unique :: Ord a => [a] -> [a]
+unique xs = Set.elems $ Set.fromList xs
+
+-- use Map to implement a function
+frequencyMap :: Ord a => [a] -> Map a Int
+frequencyMap xs =
+    Map.unionsWith (+) [Map.singleton x 1 | x <- xs]
